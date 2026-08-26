db90f9168b0b1ae3963da3d4e49c3f1604c9ff00
Ratchet <250710285+RatchetStuff@users.noreply.github.com>
2026-08-26 16:11:45 +0800

    update tool, some fixes

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+find . -type d \( -name "modrinth" -o -name "curseforge" \) -print0 | while IFS= read -r -d '' dir; do
+    (
+        cd "$dir" || exit 1
+        packwiz update -ay
+    )
+done
