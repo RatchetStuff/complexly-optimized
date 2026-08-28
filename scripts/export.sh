063a41ece36c74f7c21b833ab149d662120217a1
Ratchet <250710285+RatchetStuff@users.noreply.github.com>
2026-08-28 21:29:34 +0800

    feat: Add export.sh

---
+++ scripts/export.sh
@@ -0,0 +1,25 @@
+toplevel=$(git rev-parse --show-toplevel)
+bin_dir="$toplevel/bin"
+rm -r "$bin_dir"
+mkdir -p "$bin_dir"
+find "$toplevel/versions" -type f -name "pack.toml" -print0 | while IFS= read -r -d '' filepath; do
+    dir=$(dirname "$filepath")
+    distribution="$(basename "$dir")"
+    (
+        cd "$dir" || exit 1
+        case "$distribution" in
+        cf)
+            packwiz cf export
+            mv *.zip "$bin_dir"
+            ;;
+        mr)
+            packwiz mr export
+            mv *.mrpack "$bin_dir"
+            ;;
+        *)
+            echo "Unknown distribution: $distribution"
+            exit 1
+            ;;
+        esac
+    )
+done
