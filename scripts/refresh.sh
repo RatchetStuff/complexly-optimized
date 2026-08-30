efc0709ee6391367609ce4745cf9d60c1b258a3c
Ratchet <250710285+RatchetStuff@users.noreply.github.com>
2026-08-30 19:57:25 +0800

    feat: remove refresh.sh

--- scripts/refresh.sh
+++
@@ -1,7 +0,0 @@
-find $(git rev-parse --show-toplevel)/versions -type f -name "pack.toml" -print0 | while IFS= read -r -d '' filepath; do
-    dir=$(dirname "$filepath")
-    (
-        cd "$dir" || exit 1
-        packwiz refresh
-    )
-done
