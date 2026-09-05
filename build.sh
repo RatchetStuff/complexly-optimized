21779963469b4512b8b3810d3bd1e354594ee1ce
Ratchet <250710285+RatchetStuff@users.noreply.github.com>
2026-09-05 17:25:54 +0800

    feat: Cleanup build.sh

--- build.sh
+++ build.sh
@@ -1,25 +1,25 @@
-toplevel=$(git rev-parse --show-toplevel)
-bin_dir="$toplevel/bin"
-rm -r "$bin_dir"
-mkdir -p "$bin_dir"
-find "$toplevel/versions" -type f -name "pack.toml" -print0 | while IFS= read -r -d '' filepath; do
+TOPLEVEL=$(git rev-parse --show-TOPLEVEL)
+BIN="$TOPLEVEL/bin"
+rm -r "$BIN"
+mkdir -p "$BIN"
+find "$TOPLEVEL/versions" -type f -name "pack.toml" -print0 | while IFS= read -r -d '' filepath; do
     dir=$(dirname "$filepath")
     distribution="$(basename "$dir")"
     (
         cd "$dir" || exit 1
         case "$distribution" in
-        curseforge)
-            packwiz cf export
-            mv *.zip "$bin_dir"
-            ;;
-        modrinth)
-            packwiz mr export
-            mv *.mrpack "$bin_dir"
-            ;;
-        *)
-            echo "Unknown distribution: $distribution"
-            exit 1
-            ;;
+            curseforge)
+                packwiz cf export
+                mv *.zip "$BIN"
+                ;;
+            modrinth)
+                packwiz mr export
+                mv *.mrpack "$BIN"
+                ;;
+            *)
+                echo "Unknown distribution: $distribution"
+                exit 1
+                ;;
         esac
     )
 done
