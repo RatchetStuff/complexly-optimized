c417b179ae74f5325eeeb87992cf2dbc83f204c2
Ratchet <250710285+RatchetStuff@users.noreply.github.com>
2026-09-21 18:40:47 +0800

    refactor: Use bash in shell scripts

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,28 +1,29 @@
+#!/usr/bin/env bash
 TOPLEVEL=$(git rev-parse --show-toplevel) || exit 1
 BIN="$TOPLEVEL/bin"
-if [ -d "$BIN" ]; then
-    rm -r "$BIN"
+if [[ -d "$BIN" ]]; then
+  rm -r "$BIN"
 fi
 mkdir -p "$BIN"
 
 find "$TOPLEVEL/versions" -type f -name "pack.toml" -print0 | while IFS= read -r -d '' filepath; do
-    dir=$(dirname "$filepath")
-    distribution="$(basename "$dir")"
-    (
-        cd "$dir" || exit 1
-        case "$distribution" in
-            curseforge)
-                packwiz cf export
-                mv *.zip "$BIN"
-                ;;
-            modrinth)
-                packwiz mr export
-                mv *.mrpack "$BIN"
-                ;;
-            *)
-                echo "Unknown distribution: $distribution"
-                exit 1
-                ;;
-        esac
-    )
+  dir=$(dirname "$filepath")
+  distribution="$(basename "$dir")"
+  (
+    cd "$dir" || exit 1
+    case "$distribution" in
+      curseforge)
+        packwiz cf export
+        mv *.zip "$BIN"
+        ;;
+      modrinth)
+        packwiz mr export
+        mv *.mrpack "$BIN"
+        ;;
+      *)
+        echo "Unknown distribution: $distribution"
+        exit 1
+        ;;
+    esac
+  )
 done
